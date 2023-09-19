ea4d1e3d92f35bf2643f74366318d725135e1ac1
BT_4N2_29 <email@email.com>
2023-09-19 13:06:33 -0400

    ~added favorite food println line

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My Name is Tian Ying Chen");
         System.out.println("I dont have a favorite movie");
+        System.out.println("Chocolateee :D ");
     }
 }
